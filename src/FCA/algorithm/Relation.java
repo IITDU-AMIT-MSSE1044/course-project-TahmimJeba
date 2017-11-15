80d35182a31e6344d5b210eff38bd71a9b578805
Jeba <taj.suchana@gmail.com>
2017-11-15 10:46:55 +0600

    FCA Generation

---
+++ src/FCA/algorithm/Relation.java
@@ -0,0 +1,69 @@
+package FCA.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Relation {
+	private Object object;
+	private Attribute attribute;
+
+	public Object getObject() {
+		return object;
+	}
+
+	public void setObject(Object object) {
+		this.object = object;
+	}
+
+	public Attribute getAttribute() {
+		return attribute;
+	}
+
+	public void setAttribute(Attribute attribute) {
+		this.attribute = attribute;
+	}
+
+	private Relation() {
+	};
+
+	public static class RelationBuilder {
+		private String object;
+		private String attribute;
+
+		public RelationBuilder object(String obj) {
+			this.object = obj;
+			return this;
+		}
+
+		public RelationBuilder attribute(String attr) {
+			this.attribute = attr;
+			return this;
+		}
+
+		public Relation build() {
+			Relation relation = new Relation();
+			relation.setObject(new Object(object));
+			relation.setAttribute(new Attribute(attribute));
+			return relation;
+		}
+	}
+
+	public Relation toClone() {
+		return new RelationBuilder().object(object.objectName).attribute(attribute.getAttributeString()).build();
+	}
+
+	public static List<Relation> toCloneTransactions(List<Relation> toCloneList) {
+		List<Relation> relations = new ArrayList<Relation>();
+		for (Relation relation : toCloneList) {
+			relations.add(relation.toClone());
+		}
+		return relations;
+	}
+
+	@Override
+	public String toString() {
+		return object.objectName + "," + attribute;
+	}
+
+}
